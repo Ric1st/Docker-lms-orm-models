2714acb5bb60bbe6c89a87642d1c34165659d446
Ric1st <frenartho@gmail.com>
2025-12-10 20:57:32 +0700

    Railway16

--- code/lms_project/settings.py
+++ code/lms_project/settings.py
@@ -12,23 +12,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-+=e!(c(h$2o44pn9-x1w8qou10sx#q^l7p8q6p%s(pv$h)z3uy'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -71,10 +61,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'lms_project.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -85,9 +71,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -104,10 +87,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -119,24 +98,17 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# ===============================================
-# KONFIGURASI MEDIA UPLOAD (GAMBAR, FILE)
-# ===============================================
-
-# URL yang digunakan saat file media diakses di browser
 MEDIA_URL = '/media/'
 
-# Direktori fisik tempat file media akan disimpan
-# Ini akan membuat folder 'media' di root proyek Anda
 MEDIA_ROOT = BASE_DIR / 'media' 
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# URL yang akan dituju setelah pengguna berhasil login.
-LOGIN_REDIRECT_URL = 'home' # Arahkan ke halaman 'home' (dashboard) setelah login berhasil
+LOGIN_REDIRECT_URL = 'home' 
 
-# URL yang akan dituju setelah pengguna berhasil logout.
-LOGOUT_REDIRECT_URL = 'index' # Arahkan ke halaman 'index' setelah logout berhasil
+LOGOUT_REDIRECT_URL = 'index' 
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://docker-lms-orm-models-production.up.railway.app',
+    'https://*.up.railway.app', 
+]
